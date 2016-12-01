f00113b6d91d9e0fb765f657c7ded725a47ae056
James Smith <james@floppy.org.uk>
2016-12-01 20:22:57 +0000

    merging of cues

--- lib/vtt2an/webvtt/file.rb
+++ lib/vtt2an/webvtt/file.rb
@@ -4,4 +4,20 @@ class WebVTT::File
     cues.map{|x|x.speaker}.uniq
   end
   
+  def merged_cues
+    merged = []
+    last_speaker = nil
+    cues.each do |input_cue|
+      if input_cue.speaker != last_speaker
+        merged.last.parse unless merged.empty?
+        merged << WebVTT::Cue.new(input_cue.instance_variable_get(:@content))
+      else
+        merged.last.instance_variable_set(:@content, "#{merged.last.instance_variable_get(:@content)} #{input_cue.text}")
+      end
+      last_speaker = input_cue.speaker
+    end
+    merged.last.parse unless merged.empty?
+    merged
+  end
+  
 end
--- spec/webvtt_file_spec.rb
+++ spec/webvtt_file_spec.rb
@@ -21,4 +21,32 @@ RSpec.describe "WebVTT patches" do
 
   end
 
+  context "can merge colocated(?) cues" do
+    
+    before :all do
+      vtt = WebVTT.read("spec/fixtures/speakers.vtt")
+      @cues = vtt.merged_cues
+    end
+    
+    it "by merging cues" do
+      expect(@cues.length).to eql 5
+      expect(@cues[0].speaker).to eql "Roger Bingham"
+      expect(@cues[1].speaker).to eql "Neil deGrasse Tyson"
+      expect(@cues[2].speaker).to eql "Roger Bingham"
+      expect(@cues[3].speaker).to eql "Neil deGrasse Tyson"
+      expect(@cues[4].speaker).to eql "Roger Bingham"
+    end
+
+    it "by merging cues with the same speaker" do
+      cue = @cues[0]
+      expect(cue.text).to eql "We are in New York City We’re actually at the Lucern Hotel, just down the street from the American Museum of Natural History And with me is Neil deGrasse Tyson Astrophysicist, Director of the Hayden Planetarium at the AMNH. Thank you for walking down here. And I want to do a follow-up on the last conversation we did. When we e-mailed—"
+    end
+    
+    it "but leave individual cues unmerged" do
+      cue = @cues[1]
+      expect(cue.text).to eql "Didn’t we talk about enough in that conversation?"
+    end
+  end
+
+
 end
